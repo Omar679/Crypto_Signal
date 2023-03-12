4bd2981245fda15c47569a41f9ad2df844488707
Umar <omarsabiu679@gmail.com>
2023-03-12 08:53:09 +0100

    Setting up  Tab navigator

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Screen1 from "./screens/Screen1";
+import Screen2 from "./screens/Screen2";
+import Screen3 from "./screens/Screen3";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused
+                  ? "ios-information-circle"
+                  : "ios-information-circle-outline";
+              } else if (route.name === "Settings") {
+                iconName = focused ? "ios-list" : "ios-list-outline";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{ tabBarBadge: 3 }}
+          />
+          <Tab.Screen
+            name="Screen1"
+            component={Screen1}
+            options={{ tabBarBadge: 3 }}
+          />
+          <Tab.Screen
+            name="Screen2"
+            component={Screen2}
+            options={{ tabBarBadge: 3 }}
+          />
+          <Tab.Screen
+            name="Screen3"
+            component={Screen3}
+            options={{ tabBarBadge: 3 }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -13,8 +62,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
